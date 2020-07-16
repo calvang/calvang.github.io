8fb101e867cd0b675567d512ce16a3d1c11257cf
calvang <calvin.huang5@gmail.com>
2020-07-16 15:24:48 -0400

    Updates

---
+++ precache-manifest.07cec5a4cd03862de9fdbf6857be53ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b60225248f6eaefd0b69d2b1c29a6f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adaf167cfc99464f9b69",
+    "url": "/static/css/main.f8dfc555.chunk.css"
+  },
+  {
+    "revision": "44623ceae366cae15b48",
+    "url": "/static/js/2.26b02dfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.26b02dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaf167cfc99464f9b69",
+    "url": "/static/js/main.8dae20d0.chunk.js"
+  },
+  {
+    "revision": "db7584034fc9796da53d",
+    "url": "/static/js/runtime-main.152abb53.js"
+  },
+  {
+    "revision": "86fae41c723448ac273457b0cf4c2275",
+    "url": "/static/media/darkGlass.86fae41c.webp"
+  },
+  {
+    "revision": "32cfc0cd9db6794f19e460328367d923",
+    "url": "/static/media/metalWaves.32cfc0cd.jpg"
+  },
+  {
+    "revision": "62e31a6798dc4299b6ebae2b409176ca",
+    "url": "/static/media/neonMetal.62e31a67.webp"
+  }
+]);
